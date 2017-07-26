1554d97645d18325636c1862c67925f4c123f4ee
Unknown <orymaxence@gmail.com>
2017-07-26 21:55:04 +0200

    class form for operation

--- gestion_operations/forms.py
+++ gestion_operations/forms.py
@@ -1,1 +1,11 @@
 from django import forms
+
+class OperationForm(form.Form):
+    numeroCompte = forms.NumberInput()
+    montant = forms.NumberInput()
+    date = forms.DateInput()
+    types = forms.CharField()
+    debit = forms.BooleanField()
+    description = forms.Textarea()
+
+
