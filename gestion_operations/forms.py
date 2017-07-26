a58beaa685db30ecb0dc5b15961c9c20c119d0eb
Unknown <orymaxence@gmail.com>
2017-07-26 22:50:26 +0200

    Display of the form is corrected

--- gestion_operations/forms.py
+++ gestion_operations/forms.py
@@ -1,11 +1,10 @@
 from django import forms
 
 class OperationForm(forms.Form):
-    numeroCompte = forms.NumberInput()
-    montant = forms.NumberInput()
-    date = forms.DateInput()
-    types = forms.CharField()
+    numeroCompte = forms.CharField(widget= forms.NumberInput)
+    montant = forms.CharField(widget= forms.NumberInput)
+    date = forms.DateField(widget= forms.SelectDateWidget(years=range(2017, 2021)))
+    types = forms.CharField(max_length=100)
     debit = forms.BooleanField()
-    description = forms.Textarea()
-
+    description = forms.CharField(widget = forms.Textarea)
 
