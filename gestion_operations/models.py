5887bcea49d1fc2bd1094da8c017ec4e237fd5ca
Unknown <orymaxence@gmail.com>
2017-08-05 20:31:10 +0200

    add id to models

--- gestion_operations/models.py
+++ gestion_operations/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 
 class Compte(models.Model):
+    id = models.AutoField(primary_key=True)
     nom = models.CharField(max_length=150)
     solde = models.DecimalField(max_digits=20, decimal_places=2)
     date_fin = models.DateField(blank=True, null=True)
@@ -16,6 +17,7 @@ class Compte(models.Model):
 
 
 class Types(models.Model):
+    id = models.AutoField(primary_key=True)
     nom = models.CharField(max_length=150)
 
     class Meta:
@@ -27,6 +29,7 @@ class Types(models.Model):
 
 
 class Operation(models.Model):
+    id = models.AutoField(primary_key=True)
     compte = models.ForeignKey('Compte')
     montant = models.DecimalField(max_digits=20, decimal_places=2)
     date_ope = models.DateField(blank=True, null=True)
--- gestion_operations/views.py
+++ gestion_operations/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from gestion_operations.models import Types, Compte, Operation
 from .forms import OperationForm
 
+
 # Create your views here.
 def accueil(request):
     """ Afficher tous les articles de notre blog """
@@ -13,10 +14,14 @@ def accueil(request):
 def operation_form(request):
     form = OperationForm(request.POST or None)
     if form.is_valid():
-        numeroCompte = form.cleaned_data['numeroCompte']
+        """numeroCompte = form.cleaned_data['numeroCompte']
         montant = form.cleaned_data['montant']
         date = form.cleaned_data['date']
         types = form.cleaned_data['types']
         debit = form.cleaned_data['debit']
-        description = form.cleaned_data['description']
+        description = form.cleaned_data['description']"""
+        oper = form.save(commit=False)
+        c_type = oper.type_0
+
+        envoi = True
     return render(request, 'gestion_operations/form_ope.html', locals())
